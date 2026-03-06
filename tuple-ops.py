bee76c03d78230be6b4f985cfe7886d609987ff1
Ayanacyan <amitpacl@gmail.com>
2026-03-05 21:06:27 -0600

    Tuple operations

---
+++ tuple-ops.py
@@ -0,0 +1,18 @@
+tuplex=("tuple", False, 3.2, 1)
+print (tuplex)
+
+tuplex=(4, 6, 2, 8, 3, 1)
+print(tuplex)
+
+tuplex=tuplex+(5,)
+print(tuplex)
+
+tuple1=(50,100,150,200,250,300,350,250)
+print(tuple1.count(250))
+
+tuplex=(2, 4, 6, 7, 1)
+_slice=tuplex[3:5]
+print(_slice)
+
+_slice=tuplex[:6]
+print(_slice)
